b6aea972f6f0f985e38de27ca70890c2507d56d1
Josh Miller <jmiller686@gmail.com>
2021-08-09 18:38:34 -0500

    added next and previous buttons to toggle search page

--- src/components/QueryResultInfo/QueryResultInfo.jsx
+++ src/components/QueryResultInfo/QueryResultInfo.jsx
@@ -1,10 +1,11 @@
 import './QueryResultInfo.scss';
 
-const QueryResultInfo = ({currentSearch,data}) => {
+const QueryResultInfo = ({currentSearch,data, page}) => {
+    
     return (
         <div className="query-results-info">
             <p className="current-search">Showing results for "{currentSearch}":</p>
-            <p>Displaying page 1 of {Math.ceil(data.metadata.total_hits / 100)}</p>
+            <p>Displaying page {page} of {Math.ceil(data.metadata.total_hits / 100)}</p>
         </div>
     )
 }
